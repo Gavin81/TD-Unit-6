4a94d11e9215278b3f086c6ed0960ce5aa3823ac
Gavin81 <g_eyquem@hotmail.com>
2021-06-01 16:40:12 +0100

    Files uploaded
    Unit 6 folder and files uploaded

---
+++ unit-06/main.js
@@ -0,0 +1,32 @@
+
+var keyboard = document.getElementById('qwerty');
+var phrase = document.querySelector('phrase');
+var missed = 0;
+var phrases = ["Reincarnation", "Past-lives", "Past-life", "Regression", "Hypnosis", "Near-death experience", "After-death experience", "re-birth"];
+const overlay = document.querySelector('#overlay');
+
+
+btn__reset.addEventListener('click', (e) => {
+    if (){
+      btn__reset.toggle();
+    }
+});
+
+
+function getRandomPhraseAsArray(phrases){
+
+}
+
+
+function addPhraseToDisplay(arr){
+
+}
+
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
+
+
+const checkLetter = button => {
+
+}
